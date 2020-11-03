6557e0f352c0a3d3ca266a7729dd44b6513ec771
HDEROCHE <hugo.deroche@gmail.com>
2020-11-03 11:47:49 +0100

    function to log the user if has token on reload

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,7 +1,7 @@
 import { UserRecord, User, Wallet, WalletRecord, WalletListRecord } from './account';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Config } from 'protractor';
 
 
@@ -12,31 +12,40 @@ export class AccountService {
 
   constructor(private http: HttpClient) { }
   infos = [];
-  getUserDetails$ = (params = {id: null}, headers = {}) =>
-  this.http.get<UserRecord>('/users/' + params.id)
-       .pipe(map(p => new User(p)))
+  getUserDetails$ = (params = { id: null }, headers = {}) =>
+    this.http.get<UserRecord>('/users/' + params.id)
+      .pipe(map(p => new User(p)))
 
-  getWallet$ = (params = {user_id: null}, headers = {}) =>
-  this.http.get<WalletRecord>('/wallet/' + params.user_id)
-       .pipe(map(p => new Wallet(p)))
+  getWallet$ = (params = { user_id: null }, headers = {}) =>
+    this.http.get<WalletRecord>('/wallet/' + params.user_id)
+      .pipe(map(p => new Wallet(p)))
 
   getAllUsers$ = (params = {}, headers = {}) =>
-  this.http.get<UserRecord[]>('/users/')
+    this.http.get<UserRecord[]>('/users/')
 
   getAllWallets$ = (params = {}, headers = {}) =>
-  this.http.get<WalletListRecord>('/wallets/')
+    this.http.get<WalletListRecord>('/wallets/')
 
-  postSignin$ = (params = {}, headers = {}, body = {email: null, password: null}) =>
-  this.http.post('/users/login', body).subscribe(res => {
+  postSignin$ = (params = {}, headers = {}, body = { email: null, password: null }) =>
+    this.http.post('/users/login', body, { observe: 'response' }).pipe(
+      tap(() => localStorage.clear()),
+      tap(res => console.log('/users/login returns', res)),
+      map(res => {
 
-  // tslint:disable-next-line: forin
-    for (const key in res) {
-      console.log(res[key]);
-      this.infos.push(res[key]);
-    }
-    localStorage.setItem('user_id', this.infos[0]);
-    localStorage.setItem('token', this.infos[1]);
-    })
+        // tslint:disable-next-line: forin
+        for (const key in res.body) {
+          console.log(res.body[key]);
+          this.infos.push(res.body[key]);
+        }
+        localStorage.setItem('user_id', this.infos[0]);
+        localStorage.setItem('token', this.infos[1]);
+        console.log('resOk ' + res.ok);
+        let signInStatus = false;
+        signInStatus = res.ok;
+        console.log('post : ' + signInStatus);
+        return signInStatus;
+      }))
+
 
   postSignup$ = (params = {}, headers = {}, body = {
     name: null,
@@ -46,26 +55,21 @@ export class AccountService {
     adress: null,
     nationality: null,
     telephone: null,
-    birthDate: null}) => {
-    if (body.name !== null && body.firstname !== null && body.email !== null) {
-      this.http.post('/users/signup', body, {observe: 'response'}).subscribe(res => {
+    birthDate: null
+  }) =>
+      this.http.post('/users/signup', body, { observe: 'response' }).pipe(
+        map(res => {
         console.log(res);
         console.log(res.status);
         return res.status;
-      }, (error) => {
-        console.log(error);
-        return 500;
-      });
-    }
-    else{
-      return 501;
-    }
-  }
+      }))
 
-  checkToken = (params= {}, headers= {}, body = {user_id: null}) => {
-    if (localStorage.getItem('token') !== null && body.user_id !== null) {
-    this.http.post('/token', body);
-    }else {return ; }
+  checkToken = (params = {}, headers = {}, body = { user_id: null }) => {
+    return this.http.post('/users/token', body, { observe: 'response' }).pipe(
+      tap(_ => console.log('/users/token request')),
+      map(res => {
+        return res.body;
+      }));
   }
 }
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { AccountService } from './account.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,34 @@ export class AppComponent {
   title = 'angular-invest-easy';
 
   token = localStorage.length;
+  isConnected: boolean;
+
 
   constructor(private restAccount: AccountService) {}
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit(): void {
+    this.isLogged();
+  }
+
   isLogged(): boolean {
-    if (localStorage.getItem('token')){
-      this.restAccount.checkToken();
-    } else {
+    try {
+      if (localStorage.getItem('token')) {
+        const response$ = this.restAccount.checkToken({}, {}, {user_id: localStorage.getItem('user_id')});
+        response$.subscribe(res => {
+          if (res[Object.keys(res).find(key => key === 'token')] === localStorage.getItem('token')) {
+            console.log('result of http post : ' + true);
+            this.isConnected = true;
+          } else {
+            this.isConnected = false;
+          }
+        });
+        return this.isConnected;
+      }
+    } catch {
+      console.log('err in the isLogged function');
       return false;
     }
   }
+
 }
