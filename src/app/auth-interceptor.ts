e4485ecf1c653d7aeb2bfd23437901eebfc9d95a
HDEROCHE <hugo.deroche@gmail.com>
2020-11-03 10:57:04 +0100

    fixed interceptor header

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -7,18 +7,16 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): any {
     const BASE_URL = 'http://deroche.freeboxos.fr:8500';
-    if (request.url.startsWith('/token')) {
+    if (request.url.startsWith('/users/token')) {
     // Logged in. Add Bearer token.
-    const h: HttpHeaders = request.headers;
+    let h: HttpHeaders = request.headers;
     const token = localStorage.getItem('token');
-    if ((token !== null || token !== undefined) && typeof(token) === 'string') {
-        h.append('Authorization', token);
-    } else {return 'error in the authentification token'; }
-    console.log(h);
+    h = h.append('Authorization', 'Bearer ' + token);
+    h = h.append('Content-Type', 'application/json');
     return next.handle(request.clone({ headers: h, url: BASE_URL + request.url }));
     }
     else {
-        return next.handle(request);
+        return next.handle(request.clone({url: BASE_URL + request.url}));
     }
   }
 }
